1b86f587f9e59ab017f3a32a491ed55e55e9a765
nwlis <m.katsaragakis@hotmail.com>
2018-02-27 13:39:29 +0200

    nothing

--- mean-app/server/routes/events_api.js
+++ mean-app/server/routes/events_api.js
@@ -63,6 +63,9 @@ router.post("/createEvent", function(req, res) {
     console.log(req.body.provider)
     console.log(req.body.start_time)
     console.log(req.body.end_time)
+
+
+
     var ev = new Event({
         eventname: req.body.eventname,
         category: req.body.category,
